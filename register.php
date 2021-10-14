d1dbcd0485816954931f0f539c47854b57cdf54e
Tertius <50881039+tertiussc@users.noreply.github.com>
2021-10-14 08:58:23 +0200

    Started Register page

--- register.php
+++ register.php
@@ -8,19 +8,14 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
 
-    <title>Hello, world!</title>
+    <title>Welcome to yourFlix</title>
 </head>
 
 <body>
-<div class="container"><h1 class="display-4">Hello World</h1>
-    <div class="row">
-        <div class="col">
-
-        </div>
-    </div>
-</div>
-
+    <h1>Hello, world!</h1>
 
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
