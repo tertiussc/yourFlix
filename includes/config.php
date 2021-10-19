4609ef27a6d4acaa0153f125d46c469c4119ab94
Tertius <50881039+tertiussc@users.noreply.github.com>
2021-10-19 09:43:08 +0200

    Started config file with defaults

--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,7 @@
 <?php
 // Turn on output buffering (wait for all php code to execute before outputting it to the page)
 ob_start();
+// start the session
+session_start();
+// set timezone
+date_default_timezone_get("Africa/Johannesburg");
