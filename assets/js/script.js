0f9bfa6e599a615c25adc1b09121a9ff1400f764
Tertius <50881039+tertiussc@users.noreply.github.com>
2021-11-21 08:04:53 +0200

    Annotated to the JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+/**
+ * custom javascript functions
+ */
+
+// switches mute on and off
 function volumeToggle(button) {
     var muted = $(".preview-video").prop("muted");
     $(".preview-video").prop("muted", !muted);
@@ -6,6 +11,7 @@ function volumeToggle(button) {
     $(button).find("i").toggleClass("fa-volume-up");
 }
 
+// raplaces the preview video with a poster img after the video has finished playing
 function previewEnded() {
     $(".preview-video").toggle();
     var x = document.getElementsByClassName("preview-image");
@@ -13,10 +19,12 @@ function previewEnded() {
     // $(".preview-image").toggle();
 }
 
+// enables the use to go back to the previous page
 function goBack() {
     window.history.back();
 }
 
+// fades the back and title overlay in and out on the video player
 function startHideTimer() {
     var timeout = null;
 
@@ -30,12 +38,14 @@ function startHideTimer() {
     });
 }
 
+// initiates the fade timer and update progress
 function initVideo(videoId, username) {
     startHideTimer();
-    // call update function
+
     updateProgressTimer(videoId, username);
 }
 
+// calls the update timers and finished watching indicator 
 function updateProgressTimer(videoId, username) {
     addDuration(videoId, username);
 
@@ -45,15 +55,31 @@ function updateProgressTimer(videoId, username) {
         .on("playing", function (event) {
             window.clearInterval(timer);
             timer = window.setInterval(function () {
+                // call the update progress function
                 updateProgress(videoId, username, event.target.currentTime);
             }, 3000);
         })
         .on("ended", function () {
             window.clearInterval(timer);
+            // call the set finished function
             setFinished(videoId, username);
         });
 }
 
+// Adds a duration to the DB if a video has never been watched
+function addDuration(videoId, username) {
+    $.post(
+        "assets/ajax/add_duration.php",
+        { videoId: videoId, username: username },
+        function (data) {
+            if (data !== null && data !== "") {
+                alert(data);
+            }
+        }
+    );
+}
+
+// tracks the video playing progress
 function updateProgress(videoId, username, progress){
     $.post(
         "assets/ajax/update_duration.php",
@@ -66,6 +92,7 @@ function updateProgress(videoId, username, progress){
     );;
 }
 
+// tracks if a video has been watched finished
 function setFinished(videoId, username) {
     $.post(
         "assets/ajax/set_finished.php",
@@ -78,14 +105,4 @@ function setFinished(videoId, username) {
     );
 }
 
-function addDuration(videoId, username) {
-    $.post(
-        "assets/ajax/add_duration.php",
-        { videoId: videoId, username: username },
-        function (data) {
-            if (data !== null && data !== "") {
-                alert(data);
-            }
-        }
-    );
-}
+
